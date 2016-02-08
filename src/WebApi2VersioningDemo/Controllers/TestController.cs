8b176fcbbf48146a02789c0c1c3206c4b87ce1b6
Jakob Nordlander <jakob.nordlander@gmail.com>
2016-02-08 21:14:53 +0000

    Added basic Web Api project

---
+++ src/WebApi2VersioningDemo/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi2VersioningDemo.Controllers
+{
+    using System.Web.Http;
+
+    [RoutePrefix("api")]
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("Test")]
+        public string Test()
+        {
+            return "The api is working";
+        }
+    }
+}
---
+++ src/WebApi2VersioningDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApi2VersioningDemo
+{
+    using Microsoft.Owin.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var baseAddress = string.Format("http://localhost:{0}/", 9000);
+
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                while (true)
+                {
+
+                }
+            }
+        }
+    }
+}
